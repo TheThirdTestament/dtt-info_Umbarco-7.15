4760a324e93469045713f389d21069b91fcfc9d4
arbov <jear@eaaa.dk>
2018-03-20 21:17:24 +0100

    Booklist

--- dtt-info/Controllers/LiteratureOverviewController.cs
+++ dtt-info/Controllers/LiteratureOverviewController.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using dtt_info.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
 using Umbraco.Web;
 using Umbraco.Web.Models;
+using Umbraco.Web.PublishedContentModels;
 
 namespace dtt_info.Controllers
 {
@@ -14,13 +17,39 @@ namespace dtt_info.Controllers
         // GET: LitteratureOverview
         public override ActionResult Index(RenderModel model)
         {
-            if (RouteData.Values["category"] != null) { 
-                var category = RouteData.Values["category"].ToString();
-                
-                IPublishedContent root = Umbraco.TypedContentAtRoot().First();
-                IEnumerable<IPublishedContent> books = root.FirstChild("literatureOverview").Children().Where(x => x.IsVisible()).ToList();
+            if (RouteData.Values["category"] != null) {
 
-                return View("LiteratureList", model);
+                // url segment "category"
+                string category = RouteData.Values["category"].ToString();
+
+                LiteratureListViewModel viewModel = new LiteratureListViewModel(model.Content);
+                
+                switch (category)
+                {
+                    case "livets-bog":
+                        viewModel.Catagory = "<em>Livets Bog</em>";
+                        viewModel.Books = Umbraco.TypedContentAtRoot().First().Descendants().Where(x => x.DocumentTypeAlias == "book").Where(x => x.GetPropertyValue<int>("sortNo") > 0 && x.GetPropertyValue<int>("sortNo") < 10).Select(x => (Book)x).ToList();
+                        
+                        break;
+                    case "det-evige-verdensbillede":
+                        viewModel.Catagory = "<em>Det Evige Verdensbillede</em>";
+                        viewModel.Books = Umbraco.TypedContentAtRoot().First().Descendants().Where(x => x.DocumentTypeAlias == "book").Where(x => x.GetPropertyValue<int>("sortNo") > 10 && x.GetPropertyValue<int>("sortNo") < 20).Select(x => (Book)x).ToList();
+                        break;
+                    case "andre-store-boeger":
+                        viewModel.Catagory = "Øvrige Store Boeger";
+                        viewModel.Books = Umbraco.TypedContentAtRoot().First().Descendants().Where(x => x.DocumentTypeAlias == "book").Where(x => x.GetPropertyValue<int>("sortNo") > 20 && x.GetPropertyValue<int>("sortNo") < 50).Select(x => (Book)x).ToList();
+                        break;
+                    case "smaaboeger":
+                        viewModel.Catagory = "Småbøger";
+                        viewModel.Books = Umbraco.TypedContentAtRoot().First().Descendants().Where(x => x.DocumentTypeAlias == "book").Where(x => x.GetPropertyValue<int>("sortNo") > 100 && x.GetPropertyValue<int>("sortNo") < 150).Select(x => (Book)x).ToList();
+                        break;
+                    default:
+                        {
+                            throw new HttpException(404, "Not Found");
+                        }
+                }
+
+                return View("LiteratureList", viewModel);
             }
 
             return View("LiteratureOverview", model);
---
+++ dtt-info/ViewModels/LiteratureListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models;
+using Umbraco.Web.Models;
+using Umbraco.Web.PublishedContentModels;
+
+namespace dtt_info.ViewModels
+{
+    public class LiteratureListViewModel: RenderModel
+    {
+        public LiteratureListViewModel(IPublishedContent content) : base(content)
+        {
+            IEnumerable<Book> Books = new List<Book>();
+        }
+        public string Catagory { get; set; }
+        public IEnumerable<Umbraco.Web.PublishedContentModels.Book> Books { get; set; }
+    }
+}
