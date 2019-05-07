d350f35f5d471ed026915b1d719aa809d3361a57
arbov <jear@eaaa.dk>
2019-05-07 19:37:02 +0200

    event + css

--- dtt-info/Controllers/LiteratureOverviewController.cs
+++ dtt-info/Controllers/LiteratureOverviewController.cs
@@ -1,11 +1,8 @@
 ﻿using dtt_info.ViewModels;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Umbraco.Core.Models;
-using Umbraco.Core.Models.PublishedContent;
 using Umbraco.Web;
 using Umbraco.Web.Models;
 using Umbraco.Web.PublishedContentModels;
@@ -17,19 +14,19 @@ namespace dtt_info.Controllers
         // GET: LitteratureOverview
         public override ActionResult Index(RenderModel model)
         {
-            if (RouteData.Values["category"] != null) {
+            if (RouteData.Values["category"] != null)
+            {
 
                 // url segment "category"
                 string category = RouteData.Values["category"].ToString();
 
                 LiteratureListViewModel viewModel = new LiteratureListViewModel(model.Content);
-                
+
                 switch (category)
                 {
                     case "livets-bog":
                         viewModel.Catagory = "<em>Livets Bog</em>";
                         viewModel.Books = Umbraco.TypedContentAtRoot().First().Descendants().Where(x => x.DocumentTypeAlias == "book").Where(x => x.GetPropertyValue<int>("sortNo") > 0 && x.GetPropertyValue<int>("sortNo") < 10).Select(x => (Book)x).ToList();
-                        
                         break;
                     case "det-evige-verdensbillede":
                         viewModel.Catagory = "<em>Det evige verdensbillede</em>";
@@ -48,12 +45,9 @@ namespace dtt_info.Controllers
                             throw new HttpException(404, "Not Found");
                         }
                 }
-
                 return View("LiteratureList", viewModel);
             }
-
             return View("LiteratureOverview", model);
         }
     }
 }
-
